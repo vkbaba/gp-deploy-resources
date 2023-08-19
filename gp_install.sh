ee848ea1a6a75573ed87c86af7fa9294e3a85307
vkbaba <26544377+vkbaba@users.noreply.github.com>
2023-08-19 18:13:32 +0900

    Update gp_install.sh

--- gp_install.sh
+++ gp_install.sh
@@ -459,3 +459,86 @@ chgrp -R gpadmin /usr/local/greenplum*
 if ! entry_exists "/etc/sudoers" "gpadmin ALL=(ALL) NOPASSWD: ALL"; then
     echo "gpadmin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 fi
+
+# Deploying a Greenplum Database Cluster
+#2 Create the Greenplum GUC (global user configuration) file gp_guc_config and paste in the following contents:.
+
+MEMORY_MB=$(( MEMORY_SIZE * 1024 ))
+
+cat << EOF > /home/gpadmin/gp_guc_config
+### Interconnect Settings
+gp_interconnect_queue_depth=16
+gp_interconnect_snd_queue_depth=16
+
+# Since you have one segment per VM and less competing workloads per VM,
+# you can set the memory limit for resource group higher than the default
+gp_resource_group_memory_limit=0.85
+
+# This value should be 5% of the total RAM on the VM
+statement_mem=$(( MEMORY_MB * 5 / 100 ))MB
+
+# This value should be set to 25% of the total RAM on the VM
+max_statement_mem=$(( MEMORY_MB * 25 / 100 ))MB
+
+# This value should be set to 85% of the total RAM on the VM
+gp_vmem_protect_limit=$(( MEMORY_MB * 85 / 100 ))
+
+# Since you have less I/O bandwidth, you can turn this parameter on
+gp_workfile_compression=on
+
+# Mirrorless GUCs
+wal_level=minimal
+max_wal_senders=0
+wal_keep_segments=0
+max_replication_slots=0
+gp_dispatch_keepalives_idle=20
+gp_dispatch_keepalives_interval=20
+gp_dispatch_keepalives_count=44
+EOF
+
+chown gpadmin:gpadmin /home/gpadmin/gp_guc_config
+
+#3 Create the Greenplum configuration script create_gpinitsystem_config.sh and paste in the following contents:
+#4 Run the script to generate the configuration file for gpinitsystem. Replace 32 with the number of primary segments as necessary.
+
+# setup the gpinitsystem config
+primary_array() {
+  num_primary_segments=$1
+  array=""
+  newline=$'\n'
+  # master has db_id 0, primary starts with db_id 1, primaries are always odd
+  for i in $( seq 0 $(( num_primary_segments - 1 )) ); do
+    content_id=${i}
+    db_id=$(( i + 1 ))
+    array+="sdw${db_id}~sdw${db_id}~6000~/gpdata/primary/gpseg${content_id}~${db_id}~${content_id}${newline}"
+  done
+  echo "${array}"
+}
+
+create_gpinitsystem_config() {
+    num_primary_segments=$1
+    echo "Generate gpinitsystem"
+
+    cat <<EOF > /home/gpadmin/gpinitsystem_config
+ARRAY_NAME="Greenplum Data Platform"
+TRUSTED_SHELL=ssh
+CHECK_POINT_SEGMENTS=8
+ENCODING=UNICODE
+SEG_PREFIX=gpseg
+HEAP_CHECKSUM=on
+HBA_HOSTNAMES=0
+QD_PRIMARY_ARRAY=mdw~mdw~5432~/gpdata/master/gpseg-1~0~-1
+declare -a PRIMARY_ARRAY=(
+$( primary_array ${num_primary_segments} )
+)
+EOF
+
+}
+num_primary_segments="$SEGMENT_COUNT"
+if [ -z "$num_primary_segments" ]; then
+    echo "Usage: bash create_gpinitsystem_config.sh <num_primary_segments>"
+else
+    create_gpinitsystem_config ${num_primary_segments}
+fi
+
+chown gpadmin:gpadmin /home/gpadmin/gpinitsystem_config
